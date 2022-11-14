10f5b0a382e5403d5a2bcc9c71e8565d7cd51de5
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-15 01:37:16 +0300

    Resolve task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int nextn = 2;
+
+if (n > 1)
+{
+    while (nextn <= n)
+    {
+        Console.WriteLine(nextn + " ");
+        nextn = nextn + 2;
+    }
+}
