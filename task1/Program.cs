d3cdc90d74e733a8fe0756a317e6df2b19fcf8b3
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-15 00:53:47 +0300

    Made task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input number 1");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input number 2");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        if (n1 > n2)
+            Console.WriteLine(n1);
+        else
+            Console.WriteLine(n2);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
