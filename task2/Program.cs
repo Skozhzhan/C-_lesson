baa9c4da2dccbba0cfd1538c6d972db17c16fb5f
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-15 01:24:06 +0300

    Resolve task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+
+Console.WriteLine("Input number 1");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 3");
+int n3 = Convert.ToInt32(Console.ReadLine());
+if (n1 > max)
+{
+    max = n1;
+}
+if (n2 > max)
+{
+    max = n2;
+}
+if (n3 > max)
+{
+    max = n3;
+}
+Console.WriteLine("max = " + max);
