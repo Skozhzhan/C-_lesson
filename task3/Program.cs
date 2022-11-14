180417a1f5aa71cda3bbc6c850f577181f470f89
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-15 01:30:27 +0300

    Resolve task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input number 1");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n % 2;
+if (a == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
